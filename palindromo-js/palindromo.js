e911012f55a2a183604da2ea304659b2b24432e6
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-07 15:19:22 +0200

    add-palindrome-js

--- palindromo-js/palindromo.js
+++ palindromo-js/palindromo.js
@@ -1,3 +1,21 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindrome(word) {
+  let reversed = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  if (word === reversed) {
+    return true;
+  }
+  return false;
+}
+
+const userWord = prompt("Inserisci una parola: ");
+if (isPalindrome(userWord)) {
+  console.log("La parola è palindroma.");
+} else {
+  console.log("La parola non è palindroma.");
+}
